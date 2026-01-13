10a35456273c32cc0d969f7aa9fc62bbf3ad0934
Kashaf-Zia06 <kashafzia100@gmail.com>
2026-01-14 01:13:58 +0500

    Second UI part for application

---
+++ frontend/NotesFrontend/src/components/NoteCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {MdCreate,MdDelete} from "react-icons/md"
+import {MdOutlinePushPin} from "react-icons/md"
+
+const NoteCard = ({title,content,isPinned,date,onDelete,onEdit}) => {
+  return (
+    <div className='border rounded p-4 bg-transparent text-white hover:shadow-xl transition-all ease-in-out'>
+        <div className='flex  items-center justify-between' >
+            <div>
+                <h6 className='text-sm font-medium text-white'>{title}</h6>
+                <span className='text-white text-xs'>{date}</span>
+            </div>
+
+            <MdOutlinePushPin 
+            className={`icon-btn ${isPinned ? 'text-blue-300 ': 'text-slate-300'}`}
+            />
+
+        </div>    
+
+            <p className='text-xs mt-2 text-shadow-indigo-50'>{content?.slice(0,60)}</p>
+
+            <div className='flex items-center justify-between mt-2'>
+                <div className='flex items-center gap-2'>
+                    <MdCreate className='icon-btn hover:text-green-600' onClick={onEdit}/>
+                    <MdDelete className='icon-btn hover:text-red-500' onClick={onDelete}/>
+                </div>
+            </div>
+
+
+
+        </div>
+      
+    
+  )
+}
+
+export default NoteCard
--- frontend/NotesFrontend/src/pages/Home.jsx
+++ frontend/NotesFrontend/src/pages/Home.jsx
@@ -1,14 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '@/components/Navbar'
+import NoteCard from '@/components/NoteCard'
+import { MdAdd, MdOutlineAlarmAdd } from 'react-icons/md'
+import AddEditNotes from './AddEditNotes.jsx'
+import Modal from 'react-modal'
 
+Modal.setAppElement("#root");
 
 const Home = () => {
+
+  const [openAddEditModal,setOpenAddEditModal]=useState({
+    isShown: false,
+    type:"add",
+    data:null,
+  })
   return (
 
     <>
     <Navbar/>
-    <div className='text-white'>
-      Home
+    <div className='container mx-auto'>
+      <div className='grid grid-cols-3 gap-4 mt-8'>
+      <NoteCard 
+      title="Hello world" 
+      date="13 Jan 2026"
+      content="My first note"
+      isPinned={true}
+      onDelete={()=>{}}
+      onEdit={()=>{}}/>
+      </div>
+
+
+      <div className='grid grid-cols-3 gap-4 mt-8'>
+      <NoteCard 
+      title="Hello world" 
+      date="13 Jan 2026"
+      content="My first note"
+      isPinned={true}
+      onDelete={()=>{}}
+      onEdit={()=>{}}/>
+      </div>
+
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-[linear-gradient(90deg,_#efd5ff_0%,_#515ada_100%)] absolute right-10 bottom-10 cursor-pointer transition-all ease-in-out' 
+      onClick={()=>{
+        setOpenAddEditModal({isShown:true,type:"add" ,data:null})
+      }} >
+
+
+        <MdAdd className='text-[32px] text-white'/>
+      </button>
+
+      <Modal 
+      isOpen={openAddEditModal.isShown}
+      onRequestClose={()=>{}}
+      style={{
+        overlay: {
+          backgroundColor: "rgba(0,0,0,0.2)",
+        },
+      }}
+      contentLabel=""
+      className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+      <AddEditNotes/>
+      </Modal>
+
+
+
+
+    
+      
+      
     </div>
     </>
   )
